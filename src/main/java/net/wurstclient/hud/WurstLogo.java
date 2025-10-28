d1a7415158afe75f5bcde2ae414546ff1a19e698
Ibrahim Khurram <88876165+ibrahimkhurram407@users.noreply.github.com>
2025-10-28 05:10:39 +0500

    logo disable

--- src/main/java/net/wurstclient/hud/WurstLogo.java
+++ src/main/java/net/wurstclient/hud/WurstLogo.java
@@ -55,8 +55,8 @@ public final class WurstLogo
 		String version = "v" + WurstClient.VERSION;
 		version += " MC" + WurstClient.MC_VERSION;
 		
-		if(WURST.getUpdater().isOutdated())
-			version += " (outdated)";
+//		if(WURST.getUpdater().isOutdated())
+//			version += " (outdated)";
 		
 		return version;
 	}
